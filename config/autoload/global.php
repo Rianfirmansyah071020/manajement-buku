0e5a7af12acaeb2d898b56554d51312e407361f2
Rianfirmansyah071020 <rian071020@gmail.com>
2024-03-28 09:46:35 +0700

    mengatur controller

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -12,5 +12,13 @@
  * file.
  */
 return [
-    'db' => [],
+    'db' => [
+        'driver' => 'Pdo',
+        'dsn' => 'mysql:dbname=buku;host=localhost',
+        'username' => 'root',
+        'password' => '',
+        'driver_options' => [
+            PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''
+        ]
+    ],
 ];
--- module/Buku/src/Controller/BukuController.php
+++ module/Buku/src/Controller/BukuController.php
@@ -5,11 +5,41 @@ declare(strict_types=1);
 namespace Buku\Controller;
 
 use Laminas\Mvc\Controller\AbstractActionController;
+use Laminas\View\Model\ViewModel;
 
 class BukuController extends AbstractActionController
 {
 
-    public function ActionIndex()
+    public function indexAction()
     {
+
+        $view = new ViewModel();
+
+        $view->setTemplate('buku/buku/index');
+    }
+
+
+    public function AddAction()
+    {
+
+        $view = new ViewModel();
+
+        $view->setTemplate('buku/buku/add');
+    }
+
+
+    public function EditAction()
+    {
+        $view = new ViewModel();
+
+        $view->setTemplate('buku/buku/edit');
+    }
+
+
+    public function DeleteAction()
+    {
+        $view = new ViewModel();
+
+        $view->setTemplate('buku/buku/delete');
     }
 }
