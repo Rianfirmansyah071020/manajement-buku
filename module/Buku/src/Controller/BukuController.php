e7650d56386ac63b8edda68c6e8fe5af174e7640
Rianfirmansyah071020 <rian071020@gmail.com>
2024-03-28 09:23:59 +0700

    first commit

---
+++ module/Buku/src/Controller/BukuController.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Buku\Controller;
+
+use Laminas\Mvc\Controller\AbstractActionController;
+
+class BukuController extends AbstractActionController
+{
+
+    public function ActionIndex()
+    {
+    }
+}
